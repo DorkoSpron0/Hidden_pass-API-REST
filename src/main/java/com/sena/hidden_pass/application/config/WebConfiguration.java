bbd86d09d3b511da42aa7a8f82ac9d173c5e2d4b
dorkospron0 <tareassec9@gmail.com>
2025-03-03 10:20:38 -0500

    Adding cors

---
+++ src/main/java/com/sena/hidden_pass/application/config/WebConfiguration.java
@@ -0,0 +1,22 @@
+package com.sena.hidden_pass.application.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/v1/hidden_pass/**").allowedOrigins("*");
+            }
+        };
+    }
+
+}
