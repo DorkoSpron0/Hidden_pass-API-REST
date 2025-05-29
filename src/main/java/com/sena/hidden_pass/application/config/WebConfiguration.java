d237cdf10c89e9d9bafc338f9a26ff17c1d79cab
dorkospron0 <tareassec9@gmail.com>
2025-05-29 15:46:11 -0500

    Change allowrd origins for customs

--- src/main/java/com/sena/hidden_pass/application/config/WebConfiguration.java
+++ src/main/java/com/sena/hidden_pass/application/config/WebConfiguration.java
@@ -1,5 +1,6 @@
 package com.sena.hidden_pass.application.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,17 +9,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer(){
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/api/v1/hidden_pass/**")
-//                        .allowedOrigins("*")
-//                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//                        .allowedHeaders("*");
-//            }
-//        };
-//    }
+    @Value("${APP.CORS.ALLOWED-ORIGINS}")
+    private String allowedOrigind;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/v1/hidden_pass/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 
 }
